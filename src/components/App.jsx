61cb50c6ea25d8d0e1ef056c524a3f83287ea4f2
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-07-09 14:16:15 +0200

    add routs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Layout } from '../layout/Layout/Layout';
-import { routes } from '../routes';
-import { CocktailDetail, Cocktails, Home } from '../views';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "../layout/Layout/Layout";
+import { routes } from "../routes";
+import { CocktailDetail, Cocktails, Home } from "../views";
 
 export const App = () => {
   return (
-    <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
-      Home Page
-    </h1>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/cocktails" element={<Cocktails />} />
+        <Route path="/cocktails/:cocktailId" element={<CocktailDetail />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 };
