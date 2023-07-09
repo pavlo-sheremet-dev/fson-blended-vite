1ee3f01f7293c36b9a80fde31431813c86be6c6b
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-07-09 15:58:19 +0300

    home page completed

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,36 @@
-import { CocktailsList } from '../components/CocktailsList';
-import { Section } from '../components/Section';
-import { Loader } from '../components/Loader';
+import { useEffect, useState } from "react";
+import { CocktailsList } from "../components/CocktailsList";
+import { Section } from "../components/Section";
+import { Loader } from "../components/Loader";
+import { getTrendingCocktails } from "../api/cocktail-service";
 
 export const Home = () => {
+  const [cocktails, setCocktails] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const asyncWrapper = async () => {
+      try {
+        const trendingCocktails = await getTrendingCocktails();
+        setCocktails(trendingCocktails);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    asyncWrapper();
+  }, []);
+
   return (
     <>
       <Section>
-        <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
+        <h1 className="text-center font-black text-gray-700 text-4xl mb-10">
           Trending cocktails
         </h1>
-
-        {/* <CocktailsList cocktails={cocktails} /> */}
+        {isLoading && <Loader />}
+        <CocktailsList cocktails={cocktails} />
       </Section>
     </>
   );
