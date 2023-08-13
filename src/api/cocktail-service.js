829eed7d33fd9842eea5d20860b07978e1422c41
AlenaLarinaAL <alenalarina605@gmail.com>
2023-08-13 11:38:09 +0000

    add

--- src/api/cocktail-service.js
+++ src/api/cocktail-service.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
-const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
-axios.defaults.baseURL = 'https://www.thecocktaildb.com/api/json/v1/1';
+import axios from "axios";
+const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+axios.defaults.baseURL = "https://www.thecocktaildb.com/api/json/v1/1";
 
 const urls = Array.from({ length: 12 }, () => BASE_URL);
 
 export const getTrendingCocktails = () => {
   return Promise.all(
-    urls.map(async url => {
+    urls.map(async (url) => {
       const { data } = await axios.get(url);
       const { strDrinkThumb, strDrink, strGlass, idDrink } = data.drinks[0];
       return { strDrinkThumb, strDrink, strGlass, idDrink };
@@ -14,14 +14,22 @@ export const getTrendingCocktails = () => {
   );
 };
 
-export const getCocktailDetail = async id => {
+export const getCocktailDetail = async (id) => {
   const { data } = await axios.get(`/lookup.php?i=${id}`);
 
   return data.drinks[0];
 };
 
-export const searchByName = async query => {
+export const searchByName = async (query) => {
   const { data } = await axios.get(`/search.php?s=${query}`);
 
-  return data;
+  const list = data.drinks.map(
+    ({ strDrinkThumb, strDrink, strGlass, idDrink }) => ({
+      strDrinkThumb,
+      strDrink,
+      strGlass,
+      idDrink,
+    })
+  );
+  return list;
 };
--- src/views/Cocktails.jsx
+++ src/views/Cocktails.jsx
@@ -1,17 +1,47 @@
-import { SearchForm } from '../components/SearchForm';
-import { Section } from '../components/Section';
-import { CocktailsList } from '../components/CocktailsList';
-import { Loader } from '../components/Loader';
+import { SearchForm } from "../components/SearchForm";
+import { Section } from "../components/Section";
+import { CocktailsList } from "../components/CocktailsList";
+import { Loader } from "../components/Loader";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { searchByName } from "../api/cocktail-service";
 
 export const Cocktails = () => {
+  const [searchQuery] = useSearchParams();
+  const query = searchQuery.get("query");
+
+  const [loading, setLoading] = useState(false);
+  const [cocktails, setCocktails] = useState([]);
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+
+    async function asyncWrapper() {
+      try {
+        const cocktailsList = await searchByName(query);
+        setCocktails(cocktailsList);
+        console.log(cocktailsList);
+      } catch (error) {
+        console.log("error");
+      }
+    }
+
+    asyncWrapper();
+
+    return () => {};
+  }, [query]);
+
   return (
     <>
       <Section>
-        <h1 className='uppercase text-4xl text-gray-600 text-center'>
+        <h1 className="uppercase text-4xl text-gray-600 text-center">
           Search Cocktails
         </h1>
 
         <SearchForm />
+        <CocktailsList cocktails={cocktails} />
       </Section>
     </>
   );
